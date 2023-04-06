8c98d5ea9a6bccdc7191ecb2a20705faec01bcb4
KirillZhel <Zheltyshev.kirill@gmail.com>
2023-04-07 00:46:53 +0300

    Add project files.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WordCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
