3ab4e266970071234ae3288504fe835b38f1865a
KirillZhel <Zheltyshev.kirill@gmail.com>
2023-04-09 22:08:01 +0300

    Put Task method

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -6,5 +6,21 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public static void Task()
+        {
+            char[] buffer = new char[104857600];
+            string text = "";
+            FileStream fstream = new FileStream("document.txt", FileMode.Open, FileAccess.Read);
+            using (var sr = new StreamReader(fstream))
+            {
+                int bytesRead = 0;
+                while ((bytesRead = sr.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    text = new string(buffer);
+                    // обработка текста
+                }
+            }
+        }
     }
 }
